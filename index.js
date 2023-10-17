4cd1519c58552a5625bdefc9ea9dbf049b0fac74
muganga-charles <mugangacharls5@gmail.com>
2023-10-17 21:00:39 +0300

    add get by key

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ app.post('/:col/:key', async (req, res) => {
   res.json(item).end()
 })
 
+app.get('/:col/:key', async (req, res) => {
+  const col = req.params.col
+  const key = req.params.key
+  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
+  const item = await db.collection(col).get(key)
+  console.log(JSON.stringify(item, null, 2))
+  res.json(item).end()
+})
+
 app.get('/:col', async (req, res) => {
   const col = req.params.col
   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
