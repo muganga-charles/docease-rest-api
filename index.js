02a2fda0ce6b6164e08d975f73fef8dd29369d5f
muganga-charles <mugangacharls5@gmail.com>
2023-10-21 19:19:56 +0300

    cors middleware

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('cors');
+
 const app = express()
+
 const CyclicDB = require('@cyclic.sh/dynamodb')
 const db = CyclicDB('dull-lime-cod-robeCyclicDB')
 
 app.use(express.json())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 // app.use(express.static('public', options))
 
